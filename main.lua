c5b42216553e0f2777896c1cbd6a720ec18f7160
tbleds0727 <73623049+tbleds0727@users.noreply.github.com>
2020-10-28 21:47:46 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-myText = display.newText("TECH IN ACTION", 400, 400 native.systemFont,88)myText.setTextColor(1,0.1,0.1)
+myText = display.newText("TECH IN ACTION", 400, 400 native.systemFont,88)myText:setTextColor(1,0.1,0.1)
